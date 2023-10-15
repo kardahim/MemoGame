5899bc74950cc5abfce4fd5c30f48b4c165d895b
Paweł Ciszewski <kardahim@gmail.com>
2023-10-15 03:28:24 -0700

    Initial Commit

---
+++ MemoGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MemoGame
+//
+//  Created by Kardahim on 15/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemoGame/MemoGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoGameApp.swift
+//  MemoGame
+//
+//  Created by Kardahim on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
